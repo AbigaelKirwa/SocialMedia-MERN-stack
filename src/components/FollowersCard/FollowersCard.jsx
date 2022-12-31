350f57a7e6f871e5eae56d40f50c5b9058e57e7c
Abykirwa1937 <Abigaelkirwa1937@gmail.com>
2022-12-31 12:16:38 +0300

    first commit

---
+++ src/components/FollowersCard/FollowersCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './FollowersCard.css'
+
+const FollowersCard = () => {
+  return (
+    <div className='FollowersCard'>
+        <h3>Whos is following you</h3>
+    </div>
+  )
+}
+
+export default FollowersCard
