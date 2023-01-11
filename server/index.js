36d093d5dc5a7320a76567bf84d4901aa5bc51e0
Abykirwa1937 <Abigaelkirwa1937@gmail.com>
2023-01-11 09:47:46 +0300

    finished REST API to handle user, authentication, and post responses and requests

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
-import AuthRoute from './Routes/AuthRoute.js'
-import userRoute from './Routes/UserRoute.js'
+import dotenv from 'dotenv';
+import AuthRoute from './Routes/AuthRoute.js';
+import UserRoute from './Routes/UserRoute.js';
+import PostRoute from './Routes/PostRoute.js';
+
 // Routes
 const app = express();
 
@@ -23,4 +25,5 @@ mongoose
 
 //usage for routes
 app.use('/auth', AuthRoute)
-app.use('/user', userRoute)
+app.use('/user', UserRoute)
+app.use('/post', PostRoute)
